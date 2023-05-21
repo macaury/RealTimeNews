dc930c6e707e407fbfe589d6e88052c701d7852c
macaury carvalho <carvalhomacaury@gmail.com>
2023-05-21 02:08:31 -0300

    data_json restructures

--- python/spiders/G1_globo.py
+++ python/spiders/G1_globo.py
@@ -1,36 +1,62 @@
+import string
 import scrapy
 import json
 
 
-class G1GloboSpider(scrapy.Spider):
-    name = "G1.globo"
-    allowed_domains = ["g1.globo.com"]
-    start_urls = ["https://g1.globo.com"]
 
+def test (domain, url , noticia_rep, titulo_rep , hora_rep,img_rep ,link_rep , categoria_rep) :
+    class G1GloboSpider(scrapy.Spider):
 
-    def parse(self, response):
-        noticias = []
-        for noticia in response.css('.feed-post-body'):
-            titulo = noticia.css('.gui-color-hover a::text').get()
-            hora = noticia.css('.feed-post-datetime ::text').get()
-            imagem = noticia.css('.bstn-fd-picture-image ::attr(srcset)').get()
-            link = noticia.css('.feed-post-figure-link ::attr(href)').get()
-            categoria = noticia.css('.feed-post-metadata-section ::text').get() 
+        name = "G1.globo"
+        allowed_domains = [domain]
+        start_urls = [url]
 
-            noticia_dict = {'titulo': titulo, 'imagem': imagem , 'link': link, 'H_postado' : hora , 'categoria' : categoria}
+        def parse(self, response):
+
+            noticias = []
+            for noticia in response.css(noticia_rep):
+                titulo = noticia.css(titulo_rep).get()
+                hora = noticia.css(hora_rep).get()
+                imagem = noticia.css(img_rep).get()
+                link = noticia.css(link_rep).get()
+                categoria = noticia.css(categoria_rep).get() 
+
+                noticia_dict = {'titulo': titulo, 'imagem': imagem , 'link': link, 'H_postado' : hora , 'categoria' : categoria}
+
+                noticias.append(noticia_dict)
+
+            dados = {'noticias': noticias}
+
+            new_domain =  domain.replace(".", "_")
             
-            noticias.append(noticia_dict)
+            path_data =  f"data_json/{new_domain}.json"
 
-        dados = {'noticias': noticias}
-        
-        path_data = "data_json/g1.json"
-
-        with open(path_data, 'w', encoding='utf-8') as arquivo:
-            json.dump(dados, arquivo, ensure_ascii=False, indent=4)
+            with open(path_data, 'w', encoding='utf-8') as arquivo:
+                json.dump(dados, arquivo, ensure_ascii=False, indent=4)
 
 
-from scrapy.crawler import CrawlerProcess
+    from scrapy.crawler import CrawlerProcess
+    process = CrawlerProcess()
+    process.crawl(G1GloboSpider)
+    process.start()
 
-process = CrawlerProcess()
-process.crawl(G1GloboSpider)
-process.start()
+
+
+noticia_rep = '.feed-post-body'
+
+titulo_rep = '.gui-color-hover a::text'
+
+hora_rep = '.feed-post-datetime ::text'
+
+img_rep = '.bstn-fd-picture-image ::attr(srcset)'
+
+link_rep = '.feed-post-figure-link ::attr(href)'
+
+categoria_rep = '.feed-post-metadata-section ::text'
+
+
+domain = "g1.globo.com"
+
+url= "https://g1.globo.com"
+
+test ( domain ,url, noticia_rep, titulo_rep , hora_rep,img_rep ,link_rep , categoria_rep) 
