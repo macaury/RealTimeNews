79e4aebc0a2aa5989d78d145939c6318498954a9
macaury carvalho <carvalhomacaury@gmail.com>
2023-05-23 05:23:47 -0300

    more files
    files are create == length of Journal.json

--- Python/RealNewsPy/RealNewsPy/spiders/jour.py
+++ Python/RealNewsPy/RealNewsPy/spiders/jour.py
@@ -2,55 +2,45 @@ import string
 import scrapy
 import json
 
-
-def main(domain, url):
+def main(domains, urls):
     class G1GloboSpider(scrapy.Spider):
+        name = "g1globo_spider"
 
-        name = "G1.globo"
-        allowed_domains = [domain]
-        start_urls = [url]
+        def start_requests(self):
+            for domain, url in zip(domains, urls):
+                yield scrapy.Request(url=url, callback=self.parse, meta={'domain': domain})
 
         def parse(self, response):
-
+            domain = response.meta['domain']
             path_json_journal = "data_json/journals.json/journal.json"
 
             with open(path_json_journal, encoding='utf-8') as arquivo_json:
                 dats = json.load(arquivo_json)
-
-                #domain_rep = dats["g1.globo.com"][0]["domain_rep"]
-                noticia_rep = dats[domain][0]["noticia_rep"]
-                titulo_rep = dats[domain][0]["titulo_rep"]
-                hora_rep = dats[domain][0]["hora_rep"]
-                img_rep = dats[domain][0]["img_rep"]
-                link_rep = dats[domain][0]["link_rep"]
-                categoria_rep = dats[domain][0]["categoria_rep"]
-
-            print(dats)
+                data = dats[domain][0]
+                noticia_rep = data["noticia_rep"]
+                titulo_rep = data["titulo_rep"]
+                img_rep = data["img_rep"]
+                link_rep = data["link_rep"]
 
             noticias = []
             for noticia in response.css(noticia_rep):
                 titulo = noticia.css(titulo_rep).get()
-                hora = noticia.css(hora_rep).get()
                 imagem = noticia.css(img_rep).get()
                 link = noticia.css(link_rep).get()
-                categoria = noticia.css(categoria_rep).get()
 
-                noticia_dict = {'titulo': titulo, 'imagem': imagem,
-                                'link': link, 'H_postado': hora, 'categoria': categoria}
-
+                noticia_dict = {'titulo': titulo, 'imagem': imagem, 'link': link}
                 noticias.append(noticia_dict)
 
-            dados = {'noticias': noticias}
+            dados = {domain: noticias}
 
             new_domain = domain.replace(".", "_")
-
             path_data = f"data_json/{new_domain}.json"
 
             with open(path_data, 'w', encoding='utf-8') as arquivo:
                 json.dump(dados, arquivo, ensure_ascii=False, indent=4)
 
     from scrapy.crawler import CrawlerProcess
-
+    
     process = CrawlerProcess()
     process.crawl(G1GloboSpider)
-    process.start()
+    process.start() 
--- Python/RealNewsPy/RealNewsPy/spiders/send_func.py
+++ Python/RealNewsPy/RealNewsPy/spiders/send_func.py
@@ -1,11 +1,18 @@
+from jour import main
+import json
 import sys
 sys.path.append('./jour.py')
 
-from jour import main
 
+json_file_path = "data_json/journals.json/journal.json"
+with open(json_file_path) as file:
+    data = json.load(file)
+    domains = []
+    urls = []
+    for key in data:
+        domain = data[key][0]["domain_rep"]
+        url = data[key][0]["url_rep"]
+        domains.append(domain)
+        urls.append(url)
 
-domain = "g1.globo.com"
-
-url = "https://g1.globo.com"
-
-main(domain, url)
+main(domains, urls)
