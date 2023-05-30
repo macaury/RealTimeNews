689831ad44420bc7a6ef29ae27d0d87d14ea9fb1
macaury carvalho <carvalhomacaury@gmail.com>
2023-05-30 20:17:13 -0300

    test- up

--- venv/RealNewsPy/spiders/app.py
+++ venv/RealNewsPy/spiders/app.py
@@ -1,102 +1,63 @@
 import time
-import scrapy
 import json
 from celery import Celery
 from flask import Flask, jsonify
 import tqdm
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import requests
+from scrapy import Selector
 
 
 app = Flask(__name__)
 app.config.update(
     CELERY_BROKER_URL='redis://localhost:6379',
     CELERY_RESULT_BACKEND='redis://localhost:6379/0',
-    CELERYD_CONCURRENCY=4,
-    CELERYD_PREFETCH_MULTIPLIER=4
+    CELERYD_CONCURRENCY=10,
+    CELERYD_PREFETCH_MULTIPLIER=10
 )
 
 celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
+
 @celery.task
 def scrape_news(domains, urls):
-    class G1GloboSpider(scrapy.Spider):
-        name = "g1globo_spider"
 
-        def start_requests(self):
-            for domain, url in zip(domains, urls):
-                yield scrapy.Request(url=url, callback=self.parse, meta={'domain': domain})
+    for domain, url in zip(domains, urls):
+        response = requests.get(url)
+        selector = Selector(text=response.text)
+        
+        path_json_journal = "./journals/journal.json"
 
-        def parse(self, response):
-            domain = response.meta['domain']
-            path_json_journal = "./journals/journal.json"
+        with open(path_json_journal, encoding='utf-8') as arquivo_json:
+            dats = json.load(arquivo_json)
+            data = dats[domain][0]
+            noticia_rep = data["noticia_rep"]
+            titulo_rep = data["titulo_rep"]
+            img_rep = data["img_rep"]
+            link_rep = data["link_rep"]
 
-            with open(path_json_journal, encoding='utf-8') as arquivo_json:
-                dats = json.load(arquivo_json)
-                data = dats[domain][0]
-                noticia_rep = data["noticia_rep"]
-                titulo_rep = data["titulo_rep"]
-                img_rep = data["img_rep"]
-                link_rep = data["link_rep"]
+        count = 0
+        noticias = []
 
-            count = 0
-            noticias = []
+        for noticia in selector.css(noticia_rep):
+            titulo = noticia.css(titulo_rep).get().strip()
+            imagem = noticia.css(img_rep).get()
+            link = noticia.css(link_rep).get()
 
-            for noticia in response.css(noticia_rep):
-                titulo = noticia.css(titulo_rep).get()
-                imagem = noticia.css(img_rep).get()
-                link = noticia.css(link_rep).get()
+            noticia_dict = {'titulo': titulo, 'imagem': imagem, 'link': link}
+            noticias.append(noticia_dict)
+            count += 1
+            if count >= 2:
+                break
 
-                noticia_dict = {'titulo': titulo,
-                                'imagem': imagem, 'link': link}
-                noticias.append(noticia_dict)
-                count += 1
-                if count >= 2:
-                    break
+    new_data = {}
+    for domain, noticias in zip(domains, noticias):
+        new_domain = domain.replace(".", "_")
+        new_data[new_domain] = noticias
 
-            new_domain = domain.replace(".", "_")
-            dados = {new_domain: noticias}
-
-            # Retorna o conteúdo JSON
-            return json.dumps(dados, ensure_ascii=False, indent=4)
-
-    options = Options()
-    options.add_argument("--headless")
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
-
-    service = Service('path/to/chromedriver')  # Substitua pelo caminho correto para o executável do ChromeDriver
-
-    driver = webdriver.Chrome(service=service, options=options)
-    driver.implicitly_wait(10)
-
-    process = scrapy.crawler.CrawlerProcess(settings={
-        'DOWNLOAD_DELAY': 0,
-        'CONCURRENT_REQUESTS': 100,
-        'CONCURRENT_REQUESTS_PER_DOMAIN': 100,
-        'REACTOR_THREADPOOL_MAXSIZE': 20,
-        'AUTOTHROTTLE_ENABLED': True,
-        'AUTOTHROTTLE_START_DELAY': 0.1,
-        'AUTOTHROTTLE_MAX_DELAY': 5.0,
-        'DOWNLOADER_MIDDLEWARES': {
-            'scrapy_selenium.SeleniumMiddleware': 800,
-        },
-        'SELENIUM_DRIVER_NAME': 'chrome',
-        'SELENIUM_DRIVER_EXECUTABLE_PATH': 'path/to/chromedriver',  # Substitua pelo caminho correto para o executável do ChromeDriver
-        'SELENIUM_DRIVER_ARGUMENTS': [],
-    })
-    
-    process.crawl(G1GloboSpider, driver=driver)
-    process.start()
-
-    driver.quit()
+    # Retorna o conteúdo JSON
+    conteudo = json.dumps(new_data, ensure_ascii=False, indent=4)
+    return conteudo
 
 
 @app.route("/news", methods=['GET'])
@@ -117,12 +78,14 @@ def main():
         task = scrape_news.delay(domains, urls)
 
         progress_bar = tqdm.tqdm(total=len(domains), desc="Scraping Progress")
+        
+        print("selector e responser :" , progress_bar)
 
         while not task.ready():
             # Atualiza a barra de progresso
-            progress_bar.update(1)
+            progress_bar.update(10)
             # Aguarda um curto período de tempo antes de verificar novamente
-            time.sleep(0.1)
+            time.sleep(0.5)
 
         # Finaliza a barra de progresso
         progress_bar.close()
