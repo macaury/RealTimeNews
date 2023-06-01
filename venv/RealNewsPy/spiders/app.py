d5f1280835a363e161dd6e0d64c06ad76e7ffb13
macaury carvalho <carvalhomacaury@gmail.com>
2023-06-01 15:52:28 -0300

    Update app.py

--- venv/RealNewsPy/spiders/app.py
+++ venv/RealNewsPy/spiders/app.py
@@ -7,6 +7,16 @@ import requests
 from scrapy import Selector
 
 
+app = Flask(__name__)
+app.config.update(
+    CELERY_BROKER_URL='redis://localhost:6379',
+    CELERY_RESULT_BACKEND='redis://localhost:6379/0',
+    CELERYD_CONCURRENCY=10,
+    CELERYD_PREFETCH_MULTIPLIER=10
+)
+
+celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
 
 
 @celery.task
@@ -48,44 +58,3 @@ def scrape_news(domains, urls):
     # Retorna o conteúdo JSON
     conteudo = json.dumps(new_data, ensure_ascii=False, indent=4)
     return conteudo
-<<<<<<< Updated upstream
-
-
-@app.route("/news", methods=['GET'])
-def main():
-    with app.app_context():
-        json_file_path = "./journals/journal.json"
-        with open(json_file_path) as file:
-            data = json.load(file)
-            domains = []
-            urls = []
-            for key in data:
-                domain = data[key][0]["domain_rep"]
-                url = data[key][0]["url_rep"]
-                domains.append(domain)
-                urls.append(url)
-
-        # Inicia a tarefa de scraping usando o Celery
-        task = scrape_news.delay(domains, urls)
-
-        progress_bar = tqdm.tqdm(total=len(domains), desc="Scraping Progress")
-        
-        print("selector e responser :" , progress_bar)
-
-        while not task.ready():
-            # Atualiza a barra de progresso
-            progress_bar.update(10)
-            # Aguarda um curto período de tempo antes de verificar novamente
-            time.sleep(0.5)
-
-        # Finaliza a barra de progresso
-        progress_bar.close()
-
-        # Aguarda a conclusão da tarefa e obtém o resultado
-        return jsonify(task.get())
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-=======
->>>>>>> Stashed changes
